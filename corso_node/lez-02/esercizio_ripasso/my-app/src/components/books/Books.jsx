7ae3c53097ac23ed8fe120de5d360f4c53857157
POrtalda <paoloortalda@gmail.com>
2025-12-16 19:10:26 +0100

    ok

--- corso_node/lez-02/esercizio_ripasso/my-app/src/components/books/Books.jsx
+++ corso_node/lez-02/esercizio_ripasso/my-app/src/components/books/Books.jsx
@@ -4,7 +4,9 @@ export default function Books({books}) {
     return (
       <>
         <ul>
-            
+            {books.map(b => (
+              <li key={b.id}> {b.titolo}</li>
+            ))}
         </ul>
       </>  
     )}
